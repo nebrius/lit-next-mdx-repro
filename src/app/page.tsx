4149e03a3e06b7e8764c282f766d8289ef743419
nebrius <bryan@nebri.us>
2025-10-19 20:01:49 -0700

    Add react header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Container } from "@/components/container/container";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>React content</h1>
       <Container name="World" />
     </div>
   );
